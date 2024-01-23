d8d04774f2098f005fa00014afc85feaf5e2d0a7
alphalyblob <denakeyaz@gmail.com>
2024-01-23 18:35:44 +0100

    Feats maj

--- src/Controller/Admin/SeancesCrudController.php
+++ src/Controller/Admin/SeancesCrudController.php
@@ -3,12 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Seances;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class SeancesCrudController extends AbstractCrudController
 {
@@ -24,8 +25,8 @@ class SeancesCrudController extends AbstractCrudController
         return [
             IdField::new('id')->hideOnForm(),
             AssociationField::new('cours'),
-            DateField::new('dateDebut'),
-            DateField::new('dateFin'),
+            DateTimeField::new('dateDebut'),
+            DateTimeField::new('dateFin'),
             TextField::new('infos'),
         ];
     }
